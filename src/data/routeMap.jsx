716617fcf768a92d35d30b904a6fd09b2792b155
Boris Mokriev <66565502+bmokriev@users.noreply.github.com>
2022-04-12 18:49:10 +0300

    Remove TS syntax

--- src/data/routeMap.jsx
+++ src/data/routeMap.jsx
@@ -2,13 +2,9 @@ import MainPage from "../components/pages/MainPage";
 import routePathEnum from "../enums/routePathEnum";
 import React from "react";
 
-interface IrouteMap<T> {
-  component: React.ComponentType<T>; // Page/Component to be rendered
-  path: string; // Path grabbed from routePathEnum
-}
 const { MAIN_PAGE } = routePathEnum;
 
-export function routeMap<T>(): IrouteMap<T>[] {
+export function routeMap() {
   return [
     {
       component: <MainPage />,
