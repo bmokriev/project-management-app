65803f3297feb8a2efaf9caa929577bb1e0e6284
Federico <fedeacos045@gmail.com>
2022-04-11 22:48:44 -0300

    Icon inside input!

--- src/components/ui/inputFields/password/PasswordField.jsx
+++ src/components/ui/inputFields/password/PasswordField.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
-//import "./style.scss";
+import "./style.scss";
 
 function PasswordField() {
   const [password, setPassword] = useState("");
@@ -8,17 +8,23 @@ function PasswordField() {
 
   return (
     <>
-      <label htmlFor="password-input">Password</label>
-      <input
-        type={inputType}
-        className="button-primary"
-        placeholder="Write a Password"
-        id="password-input"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      ></input>
-      <AiOutlineEye onClick={() => setInputType("text")} />
-      <AiOutlineEyeInvisible onClick={() => setInputType("password")} />
+      <form>
+        <input
+          type={inputType}
+          placeholder="Write a Password"
+          id="password-input"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        {inputType === "password" ? (
+          <AiOutlineEye className="icon" onClick={() => setInputType("text")} />
+        ) : (
+          <AiOutlineEyeInvisible
+            className="icon"
+            onClick={() => setInputType("password")}
+          />
+        )}
+      </form>
     </>
   );
 }
