3fc76e42d0067b667c03068dc4f8035f720671ed
Federico <fedeacos045@gmail.com>
2022-04-12 14:36:03 -0300

    merged with master

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,22 +1,9 @@
 import React from "react";
-<<<<<<< HEAD
-import PasswordField from "../ui/inputFields/password/PasswordField";
-import "./app.scss";
-
-function App() {
-  return (
-    <div>
-      App
-      <PasswordField />
-    </div>
-  );
-=======
 import "./app.scss";
 import AppRouter from "../../components/base/AppRouter";
 
 function App() {
   return <AppRouter />;
->>>>>>> master
 }
 
 export default App;
