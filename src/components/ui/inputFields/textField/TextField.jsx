39e35fe584f45a55bad4c977c890d18d20712d7f
Federico <fedeacos045@gmail.com>
2022-04-12 21:53:46 -0300

    textField finished with styles

--- src/components/ui/inputFields/textField/TextField.jsx
+++ src/components/ui/inputFields/textField/TextField.jsx
@@ -4,8 +4,13 @@ import "./style.scss";
 const TextField = () => {
   return (
     <>
-      <div className="text-field">
+      <div className="text-form">
         <input className="text-input" />
+        <label className="label-text">
+          <span className="label-content">
+            What's something you are currently working on?
+          </span>
+        </label>
       </div>
     </>
   );
