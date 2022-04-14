de2be3016633897fb3a22b7c2dbcc0523adbf9a7
Federico <fedeacos045@gmail.com>
2022-04-14 09:50:26 -0300

    main page restored

--- src/components/pages/MainPage.jsx
+++ src/components/pages/MainPage.jsx
@@ -1,15 +1,9 @@
 import React from "react";
-import NameAndEmailField from "../ui/inputFields/name&email/NameAndEmailField";
-import PasswordField from "../ui/inputFields/password/PasswordField";
-import VerificationField from "../ui/inputFields/verificationField/VerificationField";
 
 const MainPage = () => {
   return (
     <div>
       <h1>Hello World</h1>
-      <VerificationField />
-      <NameAndEmailField />
-      <PasswordField />
     </div>
   );
 };
