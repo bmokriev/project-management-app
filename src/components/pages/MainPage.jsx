1f4e33a8ebe1cc9b0540ffc33e21caed21fa3929
Federico <fedeacos045@gmail.com>
2022-04-15 00:40:28 -0300

    improved Verification field

--- src/components/pages/MainPage.jsx
+++ src/components/pages/MainPage.jsx
@@ -1,20 +1,9 @@
 import React from "react";
-import PasswordField from "../ui/inputFields/PasswordField/PasswordField";
-import ConfirmPasswordField from "../ui/inputFields/ConfirmPasswordField/ConfirmPasswordField";
-import EmailField from "../ui/inputFields/EmailField/Emailfield";
-import NameField from "../ui/inputFields/NameField/NameField";
-import TextField from "../ui/inputFields/textField/TextField";
-import VerificationField from "../ui/inputFields/VerificationField/VerificationField";
+
 const MainPage = () => {
   return (
     <div>
       <h1>Hello World</h1>
-      <PasswordField />
-      <ConfirmPasswordField />
-      <EmailField />
-      <NameField />
-      <TextField />
-      <VerificationField />
     </div>
   );
 };
--- src/components/ui/inputFields/verificationField/VerificationField.jsx
+++ src/components/ui/inputFields/verificationField/VerificationField.jsx
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.scss";
 
 const VerificationField = (props) => {
+  const [otp, setOtp] = useState(new Array(4).fill(""));
+
+  const handleChange = (element, index) => {
+    if (isNaN(element.value)) return false;
+
+    setOtp([...otp.map((d, idx) => (idx === index ? element.value : d))]);
+
+    //focus next
+    if (element.nextSibling) {
+      element.nextSibling.focus();
+    }
+  };
   return (
     <>
       <div className="verification-field">
-        <input type="text" maxLength="1" required />
-        <input type="text" maxLength="1" required />
-        <input type="text" maxLength="1" required />
-        <input type="text" maxLength="1" required />
+        {otp.map((data, index) => {
+          return (
+            <input
+              type="text"
+              maxLength="1"
+              required
+              key={index}
+              value={data}
+              onChange={(e) => handleChange(e.target, index)}
+              onFocus={(e) => e.target.select()}
+            />
+          );
+        })}
       </div>
     </>
   );
