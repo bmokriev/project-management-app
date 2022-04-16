238841b692aea48b278b6f89d158d4e60eba079d
MyRoWARE <myroware@gmail.com>
2022-04-16 22:46:46 +0200

    Added inputs to Login page

--- src/components/pages/LoginPage/LoginPage.jsx
+++ src/components/pages/LoginPage/LoginPage.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import "./style.scss";
 import ContainerLayout from "../../layouts/ContainerLayout/ContainerLayout";
 import ButtonPrimary from "../../ui/buttons/ButtonPrimary/ButtonPrimary";
+import EmailAndNameField from "../../ui/inputFields/EmailAndNameField/EmailAndNamefield";
+import PasswordField from "../../ui/inputFields/PasswordField/PasswordField";
 
 const LoginPage = () => {
   return (
     <ContainerLayout>
       <div className="login-page">
         <h1 className="title">Welcome back</h1>
+        <EmailAndNameField emailAndNameLabel="Email address" />
+        <PasswordField InputLabel="Password" />
         <ButtonPrimary btnTitle="Log In" />
 
         <div class="divider">
